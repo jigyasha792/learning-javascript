e9dc2a8232c64c702a85b0bc1442fbcbc8adcc66
jigyasha balkoti <balkotijigyasha@gmail.com>
2025-12-27 22:01:44 +0545

    day2

--- firsttry.js
+++ firsttry.js
@@ -1,3 +1,10 @@
 console.log("Hello world");
 let name= "jigyasha BAlkoti";
 console.log(name);
+
+let a=10;
+let b=5;
+console.log("sum", a+b);
+console.log("difference", a-b);
+
+console.log("i am very happy to learn java script") 
