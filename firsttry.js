f659bcddc7787804c336324571aa440b727bd8bf
jigyasha balkoti <balkotijigyasha@gmail.com>
2026-01-05 08:22:00 +0545

    nothing serious

--- firsttry.js
+++ firsttry.js
@@ -7,4 +7,6 @@ let b=5;
 console.log("sum", a+b);
 console.log("difference", a-b);
 
-console.log("i am very happy to learn java script") 
+console.log("i am very happy to learn java script") ;
+console.log("i forgot what i learnt in java script");
+
